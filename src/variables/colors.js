e21ef5da59fc765698fd679c570f43b0e95940a6
Julien Henrotte <julien.henrotte@shopify.com>
2021-10-14 22:17:11 -0400

    Add Colors

--- src/variables/colors.js
+++ src/variables/colors.js
@@ -1,1 +1,11 @@
-export const colors = ['#F7B1CA', '#66FEFE', '#C79DFC', '#99F0AF', '#F6CDB1'];
+export const colors = [
+  '#F7B1CA',
+  '#66FEFE',
+  '#C79DFC',
+  '#99F0AF',
+  '#F6CDB1',
+  '#f59044',
+  '#f69dfb',
+  '#ffdc7f',
+  '#64c6ca',
+];
